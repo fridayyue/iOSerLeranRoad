7a3b78fa1e6640ebe6fb695c80ba6203d96901e7
Zach <zach@zachdemacbook-pro.local>
2018-01-18 16:53:39 +0800

    Initial Commit

---
+++ antifake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  antifake
+//
+//  Created by Zach on 2018/1/18.
+//  Copyright © 2018年 Zach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ antifake/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  antifake
+//
+//  Created by Zach on 2018/1/18.
+//  Copyright © 2018年 Zach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
